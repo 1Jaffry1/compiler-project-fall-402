e05747475247f374f3417b548d62c403ad0b5a78
Sayyed Muhammad Jaffry <muhammadj110@gmail.com>
2024-02-04 02:35:59 +0330

    macos now supported for button color

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
 import tkinter as tk
 from tkinter import messagebox
+
+import tkmacosx
+
 import mysyn
 import mylex
+from tkmacosx import Button
 
 
 class GUI:
@@ -10,6 +15,7 @@ class GUI:
         self.root = tk.Tk()
         self.root.title("SQL Create Compiler")
         self.root.geometry("800x500")
+        self.root.resizable(width=False, height=False)
         bg = tk.PhotoImage(file="bg_image.png")
         photo = tk.Label(self.root, image=bg)
         photo.place(x=0, y=0)
@@ -17,10 +23,15 @@ class GUI:
         self.label = tk.Label(self.root, text="Group: Jafari, Faghih Mousavi, Arabzadeh", font=("Arial", 18))
         self.label.pack(pady=5)
 
-        self.textbox = tk.Text(self.root, height=12,bg="black",fg="#FFFDD0" ,font=("Courier New", 20))
+        self.textbox = tk.Text(self.root, height=12,bg="#283747", fg="#FFFDD0" ,font=("Courier New", 20))
         self.textbox.pack(padx=10, pady=6)
 
-        self.run_button = tk.Button(self.root, text="▶︎", bg="white", fg="dark green" ,font=("helvetica", 15), command=self.check_code)
+        if os.name == "nt":
+            self.run_button = tk.Button(self.root, text="▶︎ RUN", bg="black", fg="light green" ,font=("helvetica", 15),
+                                    command=self.check_code)
+        else:
+            self.run_button = tkmacosx.Button(self.root, text="▶︎ RUN", bg="black", fg="light green" ,font=("helvetica", 15),
+                                    command=self.check_code)
         self.run_button.pack()
 
         self.root.mainloop()
