dbb7fcb96d27988e655f25f94ddb15836b830a5f
Sayyed Muhammad Jaffry <muhammadj110@gmail.com>
2024-02-04 01:36:15 +0330

    gui updated with response

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # import mysyn
 import tkinter as tk
 from tkinter import messagebox
+import mysyn
+import mylex
 
 
 class GUI:
     def __init__(self):
+        self.data = None
         self.root = tk.Tk()
         self.root.title("SQL Create Compiler")
         self.root.geometry("800x500")
@@ -15,17 +18,29 @@ class GUI:
         self.tb = tk.Text(self.root, height=12, font=("Courier New", 20))
         self.tb.pack(padx=10, pady=6)
 
-        self.run = tk.Button(self.root, text="RUN", font=("Arial", 15), command=self.show_message)
+        self.run = tk.Button(self.root, text="RUN", font=("Arial", 15), command=self.check_code)
         self.run.pack()
 
         self.root.mainloop()
+        
+    def run_code(self):
+        mysyn.parser.parse(self.data)
+        messagebox.showinfo(message=mylex.msg+'\n'+mysyn.msg)
+        # print(mylex.msg+'\n'+mysyn.msg)
 
-    def show_message(self):
-        print(self.tb.get('1.0', tk.END) is None)
-        #     messagebox.showinfo("No input!")
-        # else:
-        #     messagebox.showinfo(message=self.tb.get('1.0', tk.END))
-            # messagebox.showinfo(title="alert", message="No input provided")
+        
+
+    def check_code(self):
+        self.data = self.tb.get('1.0', tk.END)
+        self.data = self.data.strip().upper()
+        if self.data == "\n":
+            messagebox.showinfo(message="No input! Please Enter SQL code")
+        elif not self.data.__contains__(";"):
+            messagebox.showinfo(message="FATAL: Missing ';' at end of code")
+        else:
+            self.run_code()
+
+
 app = GUI()
 # data = ""
 # while True:
--- mylex.py
+++ mylex.py
@@ -31,6 +31,7 @@ tokens = (
     'TIME'
 )
 
+
 # Regular expression rules for simple tokens
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
@@ -38,35 +39,46 @@ t_COMMA = r','
 t_ignore = ' \t'
 t_SEMICOLON = r';'
 
-line_number = 1
+line_number = 0
+index = 0
+msg = ''
+
 def t_TYPE(t):
     r"""INT|VARCHAR(\(\d+\))?|TEXT|FLOAT|DATE|TIME|BOOLEAN|NUMBER|BIT|SMALLINT|INT|BIGINT|REAL|FLOAT|DECIMAL|CHAR|NCHAR|NVARCHAR
     |TEXT|BINARY"""
+
     return t
 
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
+
     return t
 
 
 # Error handling rule
 def t_error(t):
-    print(f"LEXICAL: Illegal character '{t.value[0]}' at line {t.lineno}")
+    global index
+    print(index)
+    global msg
     t.lexer.skip(1)
+    msg+= f"LEXICAL ERROR : Illegal character '{t.value[0]}' at line {line_number} and char {t.lexpos-index}" + '\n'
 
 
 def t_newline(t):
-    r'\n+'
+    r'\n'
     global line_number
-    line_number += len(t.value)
+    global index
+    index = t.lexpos
+    line_number += 1
 
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     if t.value.upper() in tokens:
         t.type = t.value.upper()
+
     return t
 
 
